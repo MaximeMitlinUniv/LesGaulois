ce644a2fdf6f5c563bcfbf527c1cba2284ca5164
MaximeMitlinUniv <mtm5036a@ensfsi.onmicrosoft.com>
2026-04-16 14:42:45 +0200

    invariant pre post conditions

--- src/personnages/Romains.java
+++ src/personnages/Romains.java
@@ -7,7 +7,23 @@ public class Romains {
 	public Romains(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		isInvariantVerified(force);
 	}
+	private boolean isInvariantVerified(int force) {
+		assert(force > 0);
+		return force > 0;
+	}
+	
+	private boolean isPreCoditionVerified(int forceCoup) {
+		assert(forceCoup > 0);
+		return forceCoup > 0;
+	}
+	
+	private boolean isPostCoditionVerified(int ancienneForce, int nouvelleForce) {
+		assert(ancienneForce > nouvelleForce);
+		return ancienneForce > nouvelleForce;
+	}
+	
 	public String getNom() {
 		return nom;
 	}
@@ -19,12 +35,21 @@ public class Romains {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		isPreCoditionVerified(forceCoup);
+		int ancienneForce = this.force;
 		this.force = this.force - forceCoup;
+		int nouvelleForce = this.force;
 		if(this.force < 1) {
 			parler("J'abondonne !");
 		}
 		else {
 			parler("Aie");
 		}
+		isInvariantVerified(force);
+		isPostCoditionVerified(ancienneForce, nouvelleForce);
+	}
+	
+	public static void main(String args[]) {
+		Romains minus = new Romains("Minus", 6);
 	}
 }
